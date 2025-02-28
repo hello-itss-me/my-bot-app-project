ca379e1eb5fb774852fa94aac32fba1597e88085
hello-itss-me <melkeeee46@gmail.com>
2025-02-28 11:15:26 +0300

    Initial commit

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -5,16 +5,22 @@ import { Navigate } from 'react-router-dom';
 
 export function LoginPage() {
   const { user, initialize, initialized } = useAuthStore();
-  
+
   useEffect(() => {
     if (!initialized) {
       initialize();
     }
   }, [initialize, initialized]);
-  
+
   if (initialized && user) {
     return <Navigate to="/" />;
   }
-  
-  return <AuthForm />;
+
+  return (
+    <div className="h-screen flex overflow-hidden mt-2">
+      <div className="p-4 bg-light-bg dark:bg-dark-bg transition-colors duration-300" style={{maxWidth: '800px', margin: '0 auto'}}>
+        <AuthForm />
+      </div>
+    </div>
+  );
 }
